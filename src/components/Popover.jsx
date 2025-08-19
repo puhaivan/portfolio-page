c2da22a69e5f09604791516d411aafb7846b092b
Ivan Puha <unsaightly@gmail.com>
2025-08-19 19:27:06 -0400

    fixed popover bg and text color

--- src/components/Popover.jsx
+++ src/components/Popover.jsx
@@ -9,7 +9,7 @@ function Popover({ isVisible, content, x, y }) {
 
   return createPortal(
     <div
-      className="z-[999] pointer-events-none px-2 py-1 text-[11px] rounded-md text-white bg-black/70 dark:bg-white dark:text-black shadow-md"
+      className="z-[999] pointer-events-none px-2 py-1 text-[11px] rounded-md text-white bg-black/70 dark:bg-white immersive:bg-white immersive:text-black dark:text-black shadow-md"
       style={{ position: 'fixed', left: x, top: y, transform: 'translate(-50%, 12px)' }}
     >
       {content}
